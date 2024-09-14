b834b7eecc3edb011f80e36bb9698e4a0b764c40
AlanGcDev <168142186+AlanGcDev@users.noreply.github.com>
2024-09-13 23:56:16 -0600

    false

--- App/settings.py
+++ App/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '*53H&St0]7BG;lCv{=y7Pd2c#o#s{w:pn5smlj3e#&We>5bew}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 AUTH_USER_MODEL = 'CraftCoin.CustomUser'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 LOGGING = {
