cbe1f6fd885b181019e204b04f827a55d3129b96
AlanGcDev <168142186+AlanGcDev@users.noreply.github.com>
2024-09-13 23:38:14 -0600

    db

--- App/settings.py
+++ App/settings.py
@@ -92,11 +92,14 @@ WSGI_APPLICATION = 'App.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
