e6ff1c219f2d49037202ac62e9519372f8846db4
AlanGcDev <168142186+AlanGcDev@users.noreply.github.com>
2024-09-13 23:51:36 -0600

    db

--- App/settings.py
+++ App/settings.py
@@ -92,14 +92,19 @@ WSGI_APPLICATION = 'App.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': '6fDYS8kUOqIT',
+        'HOST': 'ep-odd-morning-a45eoleu.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
 }
 
 
